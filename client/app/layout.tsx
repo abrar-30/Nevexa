1bf7081964f8c3330c0be55ffea78e67e03ff2a0
Abrar <abrarchhapavala05@gmail.com>
2025-07-14 00:48:49 +0530

    changed color

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -20,14 +20,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
           <CookieDebug />
           <Toaster />
